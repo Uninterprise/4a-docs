f232d2f56213be41f1c54107436db91a615119f8
rodrigo-gomez <terranautas@hotmail.com>
2021-11-22 14:24:04 -0500

    JC4-8
    Luz María Ballesteros y Rodrigo Gómez crean el auth-ms para el primer microservicio del proyecto

--- auth_ms/authApp/serializers/userSerializer.py
+++ auth_ms/authApp/serializers/userSerializer.py
@@ -5,4 +5,5 @@ from authApp.models.user import User
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['id', 'username', 'password', 'name', 'email', 'account']
+        fields = ['id_usuario', 'nombre_usuario', 'apellido_usuario', 'direccion_usuario', 'ciudad_usuario', 'tipo_pago_usuario', 'id_reserva_usuario']
+        
